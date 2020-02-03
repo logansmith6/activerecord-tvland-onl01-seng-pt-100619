9933f727828f7841bac3bdb3349038985e624acf
Logan Smith <logsmith06@gmail.com>
2020-02-03 21:18:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,18 +1,18 @@
 class Actor < ActiveRecord::Base
-  has_many :shows
-  has_many :characters, through: :shows
+  has_many :shows 
+  has_many :characters, through: :shows 
+
+  
 
 
 
+  def full_name 
+    self.first_name + self.last_name
+  end 
 
+  def list_roles 
 
-  def full_name
+  end 
 
-  end
-
-  def list_roles
-
-  end
-
-
+  
 end
