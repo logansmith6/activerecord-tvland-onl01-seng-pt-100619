c0093fa7bc52e7ca2fb529fb1f52f8605103d6b3
Logan Smith <logsmith06@gmail.com>
2020-02-03 21:27:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :characters 
+  belongs_to :network
+
+  def actors_list
+    actors_full_name = []
+
+    self.actors.each do |actor|
+      full_n = actor.first_name + ' ' actor.last_name 
+      actors_full_name << full_n 
+    end
+    actors_full_name 
+  end 
 end
